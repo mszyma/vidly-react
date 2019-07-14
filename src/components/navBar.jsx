b60f4fc853d44eb400066896b37ba6953bd0bd12
Mariusz Szyma <m.szyma92@gmail.com>
2019-07-15 00:25:34 +0200

    learned react routing

---
+++ src/components/navBar.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { NavLink, Link } from "react-router-dom";
+
+class NavBar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link to="/" className="navbar-brand">
+          Vidly
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <div className="navbar-nav">
+            <NavLink className="nav-link nav-item" to="/movies">
+              Movies
+            </NavLink>
+            <NavLink className="nav-link nav-item" to="/customers">
+              Customers
+            </NavLink>
+            <NavLink className="nav-link nav-item" to="rentals">
+              Rentals
+            </NavLink>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
