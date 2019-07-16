e268dc22c5c0351001de0b7e9654791e2880aaf3
Mariusz Szyma <m.szyma92@gmail.com>
2019-07-16 19:14:20 +0200

    Finished the movie form

--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Joi from "joi-browser";
 import Input from "./input";
+import Select from "./select";
 
 class Form extends Component {
   state = {
@@ -67,6 +68,21 @@ class Form extends Component {
         autoFocus
         error={errors[name]}
         type={type}
+      />
+    );
+  }
+
+  renderSelect(name, label, options) {
+    const { data, errors } = this.state;
+
+    return (
+      <Select
+        name={name}
+        value={data[name]}
+        label={label}
+        options={options}
+        onChange={this.handleChange}
+        error={errors[name]}
       />
     );
   }
